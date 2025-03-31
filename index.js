cc4661d9116458651c00a6a42f4bcf40b47c62d2
Satanik Manna <mannasatanik8@gmail.com>
2025-03-31 16:00:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello MCKV i am satanik')
+  res.send('Hello MCKVIE')
 })
 
 app.get('/courses', function (req, res) {
